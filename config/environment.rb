7f45359445ffdbf7ee0f81e939821b36e4de8e58
Eric <eric.m.lombardo@gmail.com>
2020-12-10 11:17:38 -0500

    commit tree 9b37518516222f2949846c282d9fb9be26f4f60a

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,4 @@
 # Set up Bundler here following the instructions in the README
+require 'bundler/setup'   # tells computer to use bundler to intall gems
+Bundler.require(:default, :development) # loads :default(main) and :development group
+
